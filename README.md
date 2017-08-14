04540630a96fa8ee801d0fb57c012e699cd15e7b
SaltyPopcorn <30995532+SaltyPopcorn@users.noreply.github.com>
2017-08-14 13:55:00 -0700

    My stupid ui code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # UFT
 An 18+ text adventure set in a fairy tale world.
+Writted by two losers with nothing better to do
--- main.js
+++ main.js
@@ -1,1 +1,67 @@
-console.log("We're in business!")
+var CurrentDungeon = newDungeon( //FIXME (Old dungeon def)
+[
+[1,0,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,1,1,1,1],
+[1,1,1,1,1,0,1,1,1,1],
+[1,1,1,1,1,0,1,1,1,1],
+[1,0,0,0,1,0,1,1,1,1],
+[1,0,1,0,1,0,1,1,1,1],
+[1,0,1,1,1,0,1,1,1,1],
+[1,0,0,0,0,0,1,1,1,1],
+[1,1,1,1,1,0,1,1,1,1],
+[1,1,1,1,1,0,1,1,1,1]
+])
+var player = {x:5, y:9}
+var mapcanvas = document.getElementById("mapcanvas")
+var mapcontext = mapcanvas.getContext("2d")
+function UpdateMap(){
+	for(var x = 0; x<10; x++){
+		for(var y = 0; y<10; y++){
+			if(CurrentDungeon.map[x][y].solid){ //FIXME (Old dungeon lookup)
+				mapcontext.fillStyle = "black"
+			}else{
+				mapcontext.fillStyle = "white"
+			}
+			mapcontext.fillRect(x*10,y*10,10,10)
+			
+		}
+	}
+	mapcontext.fillStyle="blue"
+	mapcontext.fillRect(player.x*10+2,player.y*10+2,6,6)
+}
+//FIXME (Old dungeon code)
+function newDungeon(arr) {
+	var dungeon = {}
+	if(arr == undefined){
+		var d = []
+		for(var x = 0; x<10; x++){
+			var z = []
+			for(var y = 0; y<10; y++){
+				z[y] = {}
+				z[y].solid = true
+			}
+			d.push(z)
+		}
+		dungeon.map = d
+		return dungeon
+	}else{
+		var d = []
+		for(var x = 0; x<10; x++){
+			var z = []
+			for(var y = 0; y<10; y++){
+				z[y] = {}
+				if(arr[y][x] == 1){
+				z[y].solid = true
+				}else{
+				z[y].solid = false
+				}
+			}
+			d.push(z)
+		}
+		dungeon.map = d
+		return dungeon
+	}
+}
+
+UpdateMap();
+console.log()
